5e76990edef39261192e07fb44cbbe5d529188be
4evrlasting <andersbrandon4@gmail.com>
2025-07-14 19:30:07 -0400

    small

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -28,7 +28,6 @@ export default function Footer() {
                 </a>
               </li>
             ))}
-          </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
